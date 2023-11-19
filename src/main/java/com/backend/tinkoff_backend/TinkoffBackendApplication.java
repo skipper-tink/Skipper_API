b0d89a58d714d8891d344d079bcb9de0bcfb5e91
backad0 <aleshkinalesh1@gmail.com>
2023-11-19 18:40:16 +0600

    Initial commit

---
+++ src/main/java/com/backend/tinkoff_backend/TinkoffBackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.tinkoff_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinkoffBackendApplication.class, args);
+    }
+
+}

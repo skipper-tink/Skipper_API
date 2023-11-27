5ec415c8b1e538906833047986910680937958fe
backad0 <aleshkinalesh1@gmail.com>
2023-11-27 21:55:09 +0600

    EmployeeGrade entity added

---
+++ src/main/java/com/backend/tinkoff_backend/entityes/EmployeeGrade.java
@@ -0,0 +1,68 @@
+package com.backend.tinkoff_backend.entityes;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "EmployeeGrade")
+public class EmployeeGrade {
+
+    @Id
+    @Column(name = "employeeGradeId", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long employeeGradeId;
+
+    @JoinColumn(name = "Employee_employeeId",
+    referencedColumnName = "employeeId", nullable = false)
+    private long Employee_employeeId;
+
+    @Column(name = "employeeGradeSpecialization", nullable = false)
+    private String employeeGradeSpecialization;
+
+    @Column(name = "employeeGradeQualification", nullable = false)
+    private String employeeGradeQualification;
+
+    public EmployeeGrade() {
+    }
+
+    public EmployeeGrade(long employeeGradeId,
+                         long employee_employeeId,
+                         String employeeGradeSpecialization,
+                         String employeeGradeQualification) {
+        this.employeeGradeId = employeeGradeId;
+        Employee_employeeId = employee_employeeId;
+        this.employeeGradeSpecialization = employeeGradeSpecialization;
+        this.employeeGradeQualification = employeeGradeQualification;
+    }
+
+    public long getEmployeeGradeId() {
+        return employeeGradeId;
+    }
+
+    public void setEmployeeGradeId(long employeeGradeId) {
+        this.employeeGradeId = employeeGradeId;
+    }
+
+    public long getEmployee_employeeId() {
+        return Employee_employeeId;
+    }
+
+    public void setEmployee_employeeId(long employee_employeeId) {
+        Employee_employeeId = employee_employeeId;
+    }
+
+    public String getEmployeeGradeSpecialization() {
+        return employeeGradeSpecialization;
+    }
+
+    public void setEmployeeGradeSpecialization(String employeeGradeSpecialization) {
+        this.employeeGradeSpecialization = employeeGradeSpecialization;
+    }
+
+    public String getEmployeeGradeQualification() {
+        return employeeGradeQualification;
+    }
+
+    public void setEmployeeGradeQualification(String employeeGradeQualification) {
+        this.employeeGradeQualification = employeeGradeQualification;
+    }
+}

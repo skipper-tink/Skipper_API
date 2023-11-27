8b1eb58ce37f473078bf9712cdf73ae3d676e269
backad0 <aleshkinalesh1@gmail.com>
2023-11-27 21:41:18 +0600

    Skill entity added

---
+++ src/main/java/com/backend/tinkoff_backend/entityes/Skill.java
@@ -0,0 +1,49 @@
+package com.backend.tinkoff_backend.entityes;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Skill")
+public class Skill {
+
+    @Id
+    @Column(name = "skillId", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long skillId;
+
+    @Column(name = "skillName", nullable = false)
+    private String skillName;
+
+    public Skill() {
+    }
+
+    public Skill(long skillId,
+                 String skillName) {
+        this.skillId = skillId;
+        this.skillName = skillName;
+    }
+
+    public long getSkillId() {
+        return skillId;
+    }
+
+    public void setSkillId(long skillId) {
+        this.skillId = skillId;
+    }
+
+    public String getSkillName() {
+        return skillName;
+    }
+
+    public void setSkillName(String skillName) {
+        this.skillName = skillName;
+    }
+
+    @Override
+    public String toString() {
+        return "Skill{" +
+                "skillId=" + skillId +
+                ", skillName='" + skillName + '\'' +
+                '}';
+    }
+}

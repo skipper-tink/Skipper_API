68e081980a9fb39715dc223b552021279309f9ae
backad0 <aleshkinalesh1@gmail.com>
2023-11-27 22:16:52 +0600

    DemandEmployee entity completed

--- src/main/java/com/backend/tinkoff_backend/entityes/DemandEmployee.java
+++ src/main/java/com/backend/tinkoff_backend/entityes/DemandEmployee.java
@@ -10,4 +10,58 @@ public class DemandEmployee {
     @Column(name = "demandEmployeeId")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long idInvolvedEmployee;
+
+    @JoinColumn(name = "Demand_demandId",
+            referencedColumnName = "demandId",
+            nullable = false)
+    private long Demand_demandId;
+
+    @JoinColumn(name = "Employee_employeeId",
+            referencedColumnName = "employeeId",
+            nullable = false)
+    private long Employee_employeeId;
+
+    public DemandEmployee() {
+    }
+
+    public DemandEmployee(long idInvolvedEmployee,
+                          long demand_demandId,
+                          long employee_employeeId) {
+        this.idInvolvedEmployee = idInvolvedEmployee;
+        Demand_demandId = demand_demandId;
+        Employee_employeeId = employee_employeeId;
+    }
+
+    public long getIdInvolvedEmployee() {
+        return idInvolvedEmployee;
+    }
+
+    public void setIdInvolvedEmployee(long idInvolvedEmployee) {
+        this.idInvolvedEmployee = idInvolvedEmployee;
+    }
+
+    public long getDemand_demandId() {
+        return Demand_demandId;
+    }
+
+    public void setDemand_demandId(long demand_demandId) {
+        Demand_demandId = demand_demandId;
+    }
+
+    public long getEmployee_employeeId() {
+        return Employee_employeeId;
+    }
+
+    public void setEmployee_employeeId(long employee_employeeId) {
+        Employee_employeeId = employee_employeeId;
+    }
+
+    @Override
+    public String toString() {
+        return "DemandEmployee{" +
+                "idInvolvedEmployee=" + idInvolvedEmployee +
+                ", Demand_demandId=" + Demand_demandId +
+                ", Employee_employeeId=" + Employee_employeeId +
+                '}';
+    }
 }

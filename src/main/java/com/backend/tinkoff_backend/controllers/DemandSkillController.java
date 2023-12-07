da767781470255c5b2db8dcdcb72f770b99a94c5
backad0 <aleshkinalesh1@gmail.com>
2023-12-07 10:36:20 +0600

    getDemandSkillById method added

--- src/main/java/com/backend/tinkoff_backend/controllers/DemandSkillController.java
+++ src/main/java/com/backend/tinkoff_backend/controllers/DemandSkillController.java
@@ -5,10 +5,7 @@ import com.backend.tinkoff_backend.services.DemandSkillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -21,5 +18,14 @@ public class DemandSkillController {
     public ResponseEntity<DemandSkill> createDemandSkill(@RequestParam DemandSkill demandSkill) {
         demandSkillService.createDemandSkill(demandSkill);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/demandSkill/{id}")
+    public ResponseEntity<DemandSkill> getDemandSKillById(@PathVariable("id") long demandSkillId) {
+        try {
+            return new ResponseEntity<>(demandSkillService.getDemandSkillById(demandSkillId), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
 }
--- src/main/java/com/backend/tinkoff_backend/services/DemandSkillService.java
+++ src/main/java/com/backend/tinkoff_backend/services/DemandSkillService.java
@@ -2,8 +2,11 @@ package com.backend.tinkoff_backend.services;
 
 import com.backend.tinkoff_backend.entities.DemandSkill;
 import com.backend.tinkoff_backend.repositories.DemandSkillRepository;
+import org.hibernate.service.spi.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class DemandSkillService {
@@ -13,5 +16,13 @@ public class DemandSkillService {
 
     public void createDemandSkill(DemandSkill demandSkill) {
         demandSkillRepository.save(new DemandSkill(demandSkill.getSkillId(), demandSkill.getDemandId()));
+    }
+
+    public DemandSkill getDemandSkillById(long demandSkillId) throws ServiceException {
+        Optional<DemandSkill> demandSkillData = demandSkillRepository.findById(demandSkillId);
+
+        if (demandSkillData.isPresent())
+            return demandSkillData.get();
+        throw new ServiceException("No such demandSkill");
     }
 }

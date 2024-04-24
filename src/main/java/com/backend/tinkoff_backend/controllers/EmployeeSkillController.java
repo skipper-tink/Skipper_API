4adb5b9701f96a0695ba26d068009c333f22d8fa
backad0 <aleshkinalesh1@gmail.com>
2024-04-24 12:02:27 +0600

    Refactor employeeSkill classes made code more readable and delete getByEmployeeId and get bySkillId method via their uselessness

--- src/main/java/com/backend/tinkoff_backend/controllers/EmployeeSkillController.java
+++ src/main/java/com/backend/tinkoff_backend/controllers/EmployeeSkillController.java
@@ -1,6 +1,8 @@
 package com.backend.tinkoff_backend.controllers;
 
 import com.backend.tinkoff_backend.entities.EmployeeSkill;
+import com.backend.tinkoff_backend.exceptions.MyInvalidArgumentException;
+import com.backend.tinkoff_backend.exceptions.MyRetrievalFailureException;
 import com.backend.tinkoff_backend.services.EmployeeSkillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -18,63 +21,41 @@ public class EmployeeSkillController {
 
     @PostMapping("/employeeSkills")
     public ResponseEntity<EmployeeSkill> createEmployeeSkill(@RequestBody EmployeeSkill employeeSkill) {
-        employeeSkillService.createEmployeeSkill(employeeSkill);
-        return new ResponseEntity<>(HttpStatus.CREATED);
+        Optional<Long> opt = employeeSkillService.createEmployeeSkill(employeeSkill);
+        if (opt.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } throw new MyInvalidArgumentException("Can't create such employeeSkill");
     }
 
     @GetMapping("/employeeSkills/{id}")
     public ResponseEntity<EmployeeSkill> getEmployeeSkillById(@PathVariable("id") long employeeSkillId) {
-        try {
-            return new ResponseEntity<>(employeeSkillService.getEmployeeSkillById(employeeSkillId), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-    }
+        Optional<EmployeeSkill> opt = employeeSkillService.getEmployeeSkillById(employeeSkillId);
 
-    @GetMapping("/employeeSkills/{employeeId}")
-    public ResponseEntity<List<EmployeeSkill>> getEmployeeSkillsByEmployeeId(@PathVariable("employeeId") long employeeId) {
-        try {
-            return new ResponseEntity<>(employeeSkillService.getEmployeeSkillsByEmployeeId(employeeId), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @GetMapping("/employeeSkills/{skillId}")
-    public ResponseEntity<List<EmployeeSkill>> getEmployeeSkillsBySkillId(@PathVariable("skillId") long skillId) {
-        try {
-            return new ResponseEntity<>(employeeSkillService.getEmployeeSkillsBySkillId(skillId), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        if (opt.isPresent()) {
+            return new ResponseEntity<>(opt.get(), HttpStatus.OK);
+        } throw new MyRetrievalFailureException("No such employeeSkill by this id");
     }
 
     @GetMapping("/employeeSkills")
     public ResponseEntity<List<EmployeeSkill>> getAllEmployeeSkills() {
-        try {
-            return new ResponseEntity<>(employeeSkillService.getAllEmployeeSKills(), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        return new ResponseEntity<>(employeeSkillService.getAllEmployeeSKills(), HttpStatus.OK);
     }
 
     @PutMapping("/employeeSkills/{id}")
-    public ResponseEntity<EmployeeSkill> updateEmployeeSkill(@PathVariable("id") long employeeSkillId, @RequestBody EmployeeSkill employeeSkill) {
-        try {
-            return new ResponseEntity<>(employeeSkillService.updateEmployeeSkill(employeeSkillId, employeeSkill), HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+    public ResponseEntity<EmployeeSkill> updateEmployeeSkill(@PathVariable("id") long employeeSkillId,
+                                                             @RequestBody EmployeeSkill employeeSkill) {
+        Optional<EmployeeSkill> opt = employeeSkillService.updateEmployeeSkill(employeeSkillId, employeeSkill);
+        if (opt.isPresent()) {
+            return new ResponseEntity<>(opt.get(), HttpStatus.OK);
+        }  throw new MyInvalidArgumentException("Update employeeSkill error");
     }
 
     @DeleteMapping("/employeeSkills/{id}")
     public ResponseEntity<EmployeeSkill> deleteEmployeeSKill(@PathVariable("id") long employeeSkillId) {
-        try {
-            employeeSkillService.deleteEmployeeSKill(employeeSkillId);
+        Optional<EmployeeSkill> opt = employeeSkillService.deleteEmployeeSKill(employeeSkillId);
+        if (opt.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+        } throw new MyInvalidArgumentException("Deletion employeeSkill error");
     }
 
     @DeleteMapping("/employeeSkills")
--- src/main/java/com/backend/tinkoff_backend/services/EmployeeSkillService.java
+++ src/main/java/com/backend/tinkoff_backend/services/EmployeeSkillService.java
@@ -1,8 +1,9 @@
 package com.backend.tinkoff_backend.services;
 
 import com.backend.tinkoff_backend.entities.EmployeeSkill;
+import com.backend.tinkoff_backend.repositories.EmployeeRepository;
 import com.backend.tinkoff_backend.repositories.EmployeeSkillRepository;
-import org.hibernate.service.spi.ServiceException;
+import com.backend.tinkoff_backend.repositories.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,66 +15,63 @@ public class EmployeeSkillService {
 
     @Autowired
     EmployeeSkillRepository employeeSkillRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    SkillRepository skillRepository;
 
-    public void createEmployeeSkill(EmployeeSkill employeeSkill) {
-        employeeSkillRepository.save(new EmployeeSkill(employeeSkill.getSkillId(), employeeSkill.getEmployeeId()));
-    }
-
-    public EmployeeSkill getEmployeeSkillById(long employeeSkillId) throws ServiceException {
-        Optional<EmployeeSkill> employeeSkillData = employeeSkillRepository.findById(employeeSkillId);
-
-        if (employeeSkillData.isPresent())
-            return employeeSkillData.get();
-        throw new ServiceException("No such employeeSkill");
-    }
-
-    public List<EmployeeSkill> getEmployeeSkillsByEmployeeId(long employeeId) throws ServiceException {
-        List<EmployeeSkill> employeeSkills = employeeSkillRepository.findAllByEmployeeId(employeeId);
-
-        if (employeeSkills.isEmpty())
-            throw new ServiceException("This employee doesn't have any skills");
-        return employeeSkills;
-    }
-
-    public List<EmployeeSkill> getEmployeeSkillsBySkillId(long skillId) throws ServiceException {
-        List<EmployeeSkill> employeeSkills = employeeSkillRepository.findAllBySkillId(skillId);
-
-        if (employeeSkills.isEmpty())
-            throw new ServiceException("Nobody have this skill");
-        return employeeSkills;
-    }
-
-    public List<EmployeeSkill> getAllEmployeeSKills() throws ServiceException {
-        List<EmployeeSkill> employeeSkills = employeeSkillRepository.findAll();
-
-        if (employeeSkills.isEmpty())
-            throw new ServiceException("No employeeSkills");
-        return employeeSkills;
-    }
-
-    public EmployeeSkill updateEmployeeSkill(long employeeSkillId, EmployeeSkill employeeSkill) throws ServiceException {
-        Optional<EmployeeSkill> employeeSkillData = employeeSkillRepository.findById(employeeSkillId);
-
-        if (employeeSkillData.isPresent()) {
-            EmployeeSkill _employeeSkill = employeeSkillData.get();
-            _employeeSkill.setEmployeeId(employeeSkill.getEmployeeId());
-            _employeeSkill.setSkillId(employeeSkill.getSkillId());
-            return employeeSkillRepository.save(_employeeSkill);
+    public Optional<Long> createEmployeeSkill(EmployeeSkill employeeSkill) {
+        if (isAlreadyExist(employeeSkill)) {
+            return Optional.of(employeeSkillRepository
+                    .save(new EmployeeSkill(employeeSkill.getSkillId(), employeeSkill.getEmployeeId()))
+                    .getId());
         }
-        throw new ServiceException("No such employeeSkill");
+        return Optional.empty();
     }
 
-    public void deleteEmployeeSKill(long employeeSkillId) throws ServiceException {
+    public Optional<EmployeeSkill> getEmployeeSkillById(long employeeSkillId) {
+        return employeeSkillRepository.findById(employeeSkillId);
+    }
+
+    public List<EmployeeSkill> getAllEmployeeSKills() {
+        return employeeSkillRepository.findAll();
+    }
+
+    public Optional<EmployeeSkill> updateEmployeeSkill(long employeeSkillId, EmployeeSkill employeeSkill) {
         Optional<EmployeeSkill> employeeSkillData = employeeSkillRepository.findById(employeeSkillId);
 
-        if (employeeSkillData.isPresent()) {
-            employeeSkillRepository.deleteById(employeeSkillId);
-            return;
-        }
-        throw new ServiceException("No such employeeSkill");
+        return employeeSkillData
+                .map(value -> paramsExist(employeeSkill) ? mergeEmplSkill(value, employeeSkill) : null)
+                .map(value -> employeeSkillRepository.save(value));
+    }
+
+    public Optional<EmployeeSkill> deleteEmployeeSKill(long employeeSkillId) {
+        Optional<EmployeeSkill> employeeSkillData = employeeSkillRepository.findById(employeeSkillId);
+
+        return employeeSkillData
+                .map(es -> {
+                    employeeSkillRepository.deleteById(employeeSkillId);
+                    return es;
+                });
     }
 
     public void deleteAllEmployeeSkills() {
         employeeSkillRepository.deleteAll();
+    }
+
+    private boolean paramsExist(EmployeeSkill employeeSkill) {
+        return employeeRepository.findById(employeeSkill.getEmployeeId()).isPresent() &&
+                skillRepository.findById(employeeSkill.getSkillId()).isPresent();
+    }
+
+    private EmployeeSkill mergeEmplSkill(EmployeeSkill value, EmployeeSkill employeeSkill) {
+        value.setEmployeeId(employeeSkill.getEmployeeId());
+        value.setSkillId(employeeSkill.getSkillId());
+        return value;
+    }
+
+    private boolean isAlreadyExist(EmployeeSkill employeeSkill) {
+        return employeeSkillRepository.findByEmployeeIdAndSkillId(employeeSkill.getEmployeeId(), employeeSkill.getSkillId())
+                .isPresent();
     }
 }

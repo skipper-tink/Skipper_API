2bb6ee9771a12f51a708ab18da9accf423a3c930
”Matvey <matizmy235@gmail.com>
2024-05-13 15:06:47 +0600

    merge conflicts solved

--- src/main/java/com/backend/tinkoff_backend/TinkoffBackendApplication.java
+++ src/main/java/com/backend/tinkoff_backend/TinkoffBackendApplication.java
@@ -2,12 +2,27 @@ package com.backend.tinkoff_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TinkoffBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TinkoffBackendApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:8000")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
     }
 
 }
